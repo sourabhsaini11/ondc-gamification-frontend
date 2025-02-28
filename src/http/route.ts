bfb3315586c60bd60c829c12104f579f6a6350cf
sourabhsaini11 <ss1841620@gmail.com>
2025-02-28 16:21:28 +0530

    fix uri

--- src/http/route.ts
+++ src/http/route.ts
@@ -2,7 +2,7 @@ import { LoginFormData, RegisterFormData } from '@/types'
 import axiosInstance from '../lib/axiosInstance'
 
 export const loginAPI = async (data: LoginFormData) => {
-  const response = await axiosInstance.post('/api/v1//users/login', data)
+  const response = await axiosInstance.post('/api/v1/users/login', data)
   return response.data
 }
 
