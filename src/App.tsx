98d2892f2994b58a6c68d86783e6fea2e577d9fa
sourabhsaini11 <ss1841620@gmail.com>
2025-03-17 15:35:06 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import { AuthProvider, useAuth } from './services/AuthContext'
 import UserDashboard from './components/UserDashboard'
 import LoginPage from '../src/app/login/page'
 import SignUpPage from './components/SignUpForm'
-import NewLeaderBoardComponent from './components/NewLeaderBoardComponent'
-import { TableDemo } from './components/Table'
-import Leaderboard from './components/Leaderboard'
+
 
 function ProtectedRoute({ children }: { children: JSX.Element }) {
   const { isAuthenticated } = useAuth()
@@ -50,15 +48,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route
-          path="/temp"
-          element = {
          
-
-              <Leaderboard />
-      
-        }
-          />
      
 
           
