ebf52d09ab8442e63834f35f3a137590f63b6181
Sourabh Saini <sourabh.saini@thewitslab.com>
2025-03-20 15:40:44 +0530

    Update axiosInstance.ts

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:8000`,
+  baseURL: `https://gamafication-node-backend-dev.thewitslab.com`,
 
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
